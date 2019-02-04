79f413c174e08aed931bd58bb37f071c7f9d5fcc
Dimitar <mailto.dimitrov@gmail.com>
2019-02-05 00:24:06 +0200

    Init local authentication strategy

--- src/api.js
+++ src/api.js
@@ -1,13 +1,56 @@
 import http from 'http';
 import express from 'express';
 import cors from 'cors';
+import { urlencoded, json } from 'body-parser';
+
+import Passport from 'passport';
+import { Strategy } from 'passport-local';
+import Account from './account/account.model';
+
+const configureAuth = (options) => {
+    const localOptions = {
+        session: false,
+        passReqToCallback: true,
+        usernameField: 'email',
+        passwordField: 'password',
+    };
+
+    Passport.use('local', new Strategy(localOptions, (req, username, password, done) => {
+        Account.findOne({ email: username })
+            .then((foundUser) => {
+                if (!foundUser) {
+                    return done({ message: 'Unauthenticated' });
+                }
+
+                foundUser.comparePasswords(password, foundUser.password)
+                    .then((passwordMatch) => {
+                        if (!passwordMatch) {
+                            return done({ message: 'Invalid credentials' });
+                        }
+
+                        const userData = {
+                            // todo: token
+                            username: user.username,
+                            email: user.email,
+                        }
+
+                        return done(null, userData);
+                    })
+            });
+    }));
+}
+
+const configureMiddlewares = (api) => {
+    api
+        .use(cors())
+        .use(urlencoded({ extended: false }))
+        .use(json())
+        .use(Passport.initialize());
+}
 
 const configureApi = (api) => {
-    return new Promise((resolve, reject) => {
-        api.use(cors);
-
-        resolve(api);
-    })
+    configureAuth();
+    configureMiddlewares(api);
 }
 
 export default {
@@ -17,11 +60,10 @@ export default {
      * @param {String} port 
      */
     start(host, port) {
-        return configureApi(express())
-            .then(() => {
-                http
-                    .createServer(express)
-                    .listen(port, host);
-            });
+        configureApi(express());
+
+        return http
+            .createServer(express)
+            .listen(port, host);
     }
 };
