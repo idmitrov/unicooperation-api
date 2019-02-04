31931d7735eafc60c6f399833b54e836c5369eed
Dimitar <mailto.dimitrov@gmail.com>
2019-02-04 23:09:57 +0200

    Add doc

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,27 @@
-import { genSalt, hash } from 'bcrypt';
+import { genSalt, hash, compare } from 'bcrypt';
 
 export default {
+    /**
+     * Generate salt
+     * @param {Number} factor 
+     */
     generateSalt(factor = 10) {
         return genSalt(factor);
     },
+    /**
+     * Generate hash
+     * @param {Number} data 
+     * @param {Number} salt 
+     */
     generateHash(data, salt) {
         return hash(data, salt);
+    },
+    /**
+     * Compare 2 hashes
+     * @param {Number} candidateData 
+     * @param {Number} data 
+     */
+    compareHashes(candidateData, data) {
+        return compare(candidateData, data);
     }
 }
