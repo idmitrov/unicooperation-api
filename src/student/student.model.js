763c0efecd992478795cf114cd28efc99ed9c3d0
Dimitar <mailto.dimitrov@gmail.com>
2019-02-04 22:37:40 +0200

    Extend student model and add doc

--- src/student/student.model.js
+++ src/student/student.model.js
@@ -1,7 +1,27 @@
 import { dbSchema, dbModel } from '../db';
 
 const studentSchema = new dbSchema({
+    /**
+     * @name facultyId
+     * @type String
+     */
+    facultyId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    /**
+     * @name firstName
+     * @type String
+     */
     firstName: {
+        type: String
+    },
+    /**
+     * @name lastName
+     * @type String
+     */
+    lastName: {
         type: String
     }
 });
