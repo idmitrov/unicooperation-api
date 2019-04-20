73de3afa7145cc41e7e8fa6728294f7b06fe3f4d
Dimitar <mailto.dimitrov@gmail.com>
2019-04-20 22:32:00 +0300

    Add doc

--- src/student/student.service.js
+++ src/student/student.service.js
@@ -1,11 +1,25 @@
 import Student from './student.model';
 
 export default {
+    /**
+     * Find a student by id
+     * @name findById
+     * @param {String} id 
+     * @param {Array} projection 
+     */
     findById(id, projection = []) {
         return Student
             .findById(id)
             .select(projection);
     },
+    /**
+     * Create a new student
+     * @name create
+     * @param {String} firstName 
+     * @param {String} facultyId 
+     * @param {String} universityId 
+     * @param {String} accountId 
+     */
     create(firstName, facultyId, universityId, accountId) {        
         const newStudent = new Student({ firstName, facultyId, universityId, accountId });
 
