7a5c9f1f72569067be7e350f67b1d7ca583b92d1
Dimitar <mailto.dimitrov@gmail.com>
2019-04-24 21:19:53 +0300

    Extract socketio methods  in socket.js

--- src/publication/publication.events.js
+++ src/publication/publication.events.js
@@ -1,31 +1,7 @@
 import { accountType } from '../account/account.constants';
 import studentService from '../student/student.service';
-import publicationService from './publication.service';
 
-const rooms = {};
-
-/**
- * Add a socket to room
- * @name addClientToRoom
- * @param {String} roomId 
- * @param {Soc} client 
- */
-const addClientToRoom = (roomId, client) => {
-    rooms[roomId] ? rooms[roomId].push(client) : rooms[roomId] = [client];
-}
-
-/**
- * Remove a socket from room
- * @name addClientToRoom
- * @param {String} roomId 
- * @param {Soc} client 
- */
-const removeClientFromRoom = (roomId, clientId) => {
-    const room = rooms[roomId];
-    const clientToRemoveIndex = room.findIndex((c) => client.id === clientId);
-
-    room.splice(clientToRemoveIndex, 1);
-}
+import { addClientToRoom } from '../socket';
 
 export default (client) => {
     client.on('join', () => {
---
+++ src/socket.js
@@ -0,0 +1,24 @@
+const rooms = {};
+
+/**
+ * Add a socket to room
+ * @name addClientToRoom
+ * @param {String} roomId 
+ * @param {Soc} client 
+ */
+export const addClientToRoom = (roomId, client) => {
+    rooms[roomId] ? rooms[roomId].push(client) : rooms[roomId] = [client];
+}
+
+/**
+ * Remove a socket from room
+ * @name addClientToRoom
+ * @param {String} roomId 
+ * @param {Soc} client 
+ */
+export const removeClientFromRoom = (roomId, clientId) => {
+    const room = rooms[roomId];
+    const clientToRemoveIndex = room.findIndex((c) => client.id === clientId);
+
+    room.splice(clientToRemoveIndex, 1);
+}
