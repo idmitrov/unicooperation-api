e319ec4c1267655f6838089f25ca273d7704937d
Dimitar <mailto.dimitrov@gmail.com>
2019-06-20 22:47:02 +0300

    Test prod env

--- src/api.js
+++ src/api.js
@@ -131,6 +131,8 @@ export default {
 
         const sokcetIO = io(server,  { origins: Config.api.origins });
         server.listen(port, host, () => {
+            console.log(`listening on port ${port}`);
+            
             configureSockets(sokcetIO);
         });
     }
