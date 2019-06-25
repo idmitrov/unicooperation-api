5cd7ba38613a7b7e41855424a7ad1073db066a73
Dimitar <mailto.dimitrov@gmail.com>
2019-06-26 00:57:06 +0300

    Implement create add and tweak getAdds

--- src/add/add.controller.js
+++ src/add/add.controller.js
@@ -14,6 +14,13 @@ export default {
             .catch((error) => next({ message: error.errmsg || error }));
     },
     createNewAdd(req, res, next) {
+        const { title, content }= req.body;
+        const author = req.account;
 
+        addService.createNewAdd(title, content, author)
+            .then((createdAdd) => {
+                return res.json({ data: createdAdd });
+            })
+            .catch((error) => next({ message: error.errmsg || error }));
     }
 }
--- src/add/add.service.js
+++ src/add/add.service.js
@@ -1,16 +1,29 @@
 import Add from './add.model';
 
+const defaultAddsSkip = 0;
+const defaultAddsLimit = 10;
+const defaultAddsSort = 'createdAt';
+
 export default {
-    getAdds(page = 1, limit = 10, active = true, projection = []) {
+    getAdds(skip = defaultAddsSkip, limit = defaultAddsLimit, sort = defaultAddsSort, active = true, projection = []) {
+        const skipNumeric = parseInt(skip);
+        const limitNumeric = parseInt(limit);
+        
         return Promise.all([
             Add
                 .find({ isActive: active })
                 .select(projection)
-                .skip(page * limit)
-                .limit(limit),
+                .sort(`-${sort}`)
+                .skip(skipNumeric || defaultAddsSkip)
+                .limit(limitNumeric || defaultAddsLimit),
             Add
                 .find({ isActive: active })
                 .countDocuments()
         ]);
-    }   
+    },
+    createNewAdd(title, content, author) {
+        const add = new Add({ title, content, author });
+
+        return add.save();
+    }
 }
