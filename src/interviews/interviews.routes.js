64833da4fd6066d39c4e5211ec054b7d6493c7c7
Dimitar <mailto.dimitrov@gmail.com>
2019-07-14 00:49:58 +0300

    Add interview GET route

--- src/interviews/interviews.routes.js
+++ src/interviews/interviews.routes.js
@@ -8,6 +8,7 @@ const router = new Router();
 
 router
     .get('/mine',  [auth([accountType.partner, accountType.student])], interviewsController.getMineInterviews)
+    .get('/:interviewId',  [auth([accountType.partner, accountType.student])], interviewsController.get)
     .post('/request', [auth(accountType.partner)], interviewsController.create)
     .put('/accept', auth(accountType.student), interviewsController.accept)
     .put('/complete', auth(accountType.partner), interviewsController.complete)
