7b64cda7fe72c758af8ec635b8f11a28d94f45b8
Dimitar <mailto.dimitrov@gmail.com>
2019-07-17 22:28:50 +0300

    Tweak cooperation create

--- src/cooperations/cooperations.controller.js
+++ src/cooperations/cooperations.controller.js
@@ -1,14 +1,18 @@
 import cooperationsService from './cooperations.service';
+import studentService from '../student/student.service';
 
 export default {
     create(req, res, next) {
         const { account } = req;
-        const { university, student } = req.body;
+        const { interviewId, studentId } = req.body;
 
-        cooperationsService.create(university, account.profile, student)
-            .then((createdCooperation) => {
-                res.json({ data: createdCooperation });
-            })
-            .catch((error) => next({ message: error.errmsg || error }));
+        return studentService.findById(studentId)
+            .then((student) => {
+                cooperationsService.create(student.university, account.profile, studentId, interviewId)
+                    .then((createdCooperation) => {
+                        res.json({ data: createdCooperation });
+                    })
+                    .catch((error) => next({ message: error.errmsg || error }));
+            });
     }
 };
--- src/cooperations/cooperations.service.js
+++ src/cooperations/cooperations.service.js
@@ -7,9 +7,10 @@ export default {
      * @param {String} university The id of the university
      * @param {String} partner The id of the partner
      * @param {String} student The id of the student
+     * @param {String} interview The id of the student (nullable)
      */
-    create(university, partner, student) {
-        const cooperation = new Cooperation({ university, partner, student });
+    create(university, partner, student, interview = null) {
+        const cooperation = new Cooperation({ university, partner, student, interview });
 
         return cooperation.save();
     }
